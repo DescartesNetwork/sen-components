50f9855a47c3f57b2b4d7615a3ff0a0d32956164
tuphan-dn <tuphan@descartes.network>
2022-06-01 11:36:06 +0700

    add chunks name

--- packages/craco-plugins/src/craco-module-federation.js
+++ packages/craco-plugins/src/craco-module-federation.js
@@ -16,6 +16,9 @@ const overrideWebpackConfig = ({ context, webpackConfig, pluginOptions }) => {
   // Avoid remote collision
   // https://webpack.js.org/concepts/module-federation/#collision-between-modules-from-different-remotes
   webpackConfig.output.uniqueName = pluginOptions.uniqueName
+  // Keep the real chunk names
+  // https://stackoverflow.com/questions/66898602/webpack-module-federation-changes-names-of-shared-libraries-to-numbers
+  webpackConfig.optimization.chunkIds = 'named'
 
   // React Refresh
   // https://github.com/pmmmwh/react-refresh-webpack-plugin/issues/394#issuecomment-877708732
